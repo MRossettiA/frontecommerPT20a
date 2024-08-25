d8b2b78d3d3bbfd534ba01f778bb90c08f59c229
laugthon <rossettia_martin@hotmail.com>
2024-08-25 16:35:37 -0300

    vakidacion de emoji se agrega target

--- src/helpers/validateRegister.tsx
+++ src/helpers/validateRegister.tsx
@@ -3,22 +3,27 @@ import { IRegisterError, IRegisterProps } from "../components/register/TypesRegi
 export function validateRegisterForm(values: IRegisterProps): IRegisterError {
   const errors: IRegisterError = {};
   
+  // Validación del email
   if (values.email && !/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(values.email)) {
     errors.email = "Invalid email.";
   }
 
-  if (values.name && !/^[a-zA-Z\s]+$/.test(values.name)) {
-    errors.name = "Name cannot contain numbers or symbols.";
+  // Validación del nombre (permitir letras, espacios y emojis)
+  if (values.name && !/^[\p{L}\p{Emoji}\s]+$/u.test(values.name)) {
+    errors.name = "Name cannot contain numbers or other symbols.";
   }
+
+  // Verificación adicional de espacios consecutivos
   if (!values.name.trim()) {
-  
+    errors.name = "Name cannot be empty.";
   } else if (/\s{2,}/.test(values.name)) {
     errors.name = "Name cannot contain more than one consecutive space.";
   }
 
+  // Validación del teléfono (solo números)
   if (values.phone && !/^\d+$/.test(values.phone)) {
     errors.phone = "Phone must be a number.";
   }
   
   return errors;
-};
+}
