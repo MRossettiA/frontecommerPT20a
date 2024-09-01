77fb92c9377674f8dbf534aba58717e2757b5b47
MRossettiA <146037689+MRossettiA@users.noreply.github.com>
2024-09-01 00:18:37 -0300

    Add files via upload
    se cambio <p>No products found for &quot;{searchProduct}&quot;</p>

---
+++ page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useSearchContext } from "@/context/SearchContext"; 
+import Link from "next/link";
+import Card from "@/components/Card/Card";
+
+const SearchProduct = () => {
+  const { products, loading, searchProduct } = useSearchContext();
+
+  return (
+    <div className="p-4">
+      {loading ? (
+        <p>Loading...</p>
+      ) : products.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {products.map((product) => (
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <Card
+                id={product.id}
+                name={product.name}
+                price={product.price}
+                image={product.image}
+                stock={product.stock}
+                categoryId={product.categoryId}
+                description={product.description}
+              />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p>No products found for &quot;{searchProduct}&quot;</p>
+
+      )}
+    </div>
+  );
+};
+
+export default SearchProduct;
