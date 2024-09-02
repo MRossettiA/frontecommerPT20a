d77338e8b316089958fbefbb30ed91d9bd03e98e
MRossettiA <146037689+MRossettiA@users.noreply.github.com>
2024-09-02 09:51:09 -0300

    Update page.tsx

--- src/app/search/[searchProduct]/page.tsx
+++ src/app/search/[searchProduct]/page.tsx
@@ -28,7 +28,7 @@ const SearchProduct = () => {
           ))}
         </div>
       ) : (
-        <p>No products found for "{searchProduct}"</p>
+        <p>No products found for &ldquo;{searchProduct}&rdquo;</p>
       )}
     </div>
   );
